9d52686694565c4f791dc42a7d4d042c2beb1f6b
EN Chu <chuiantw1212@gmail.com>
2026-03-11 10:56:37 +0800

    add project

---
+++ src/test/java/com/oingg/screenings/ScreeningServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oingg.screenings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreeningServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
